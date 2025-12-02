7449ede3c9bc1449c6c1bc30a6f765b1f7494995
Gabriel Henrique <gabriel55237@gmail.com>
2025-12-02 07:12:23 -0300

    primeiro teste automatizado

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,14 @@
+/// <reference type="cypress"/>
+
+describe('Funcionalidade: Login', () => {
+
+    it('Deve fazer login com sucesso', () => {
+        cy.visit ('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('gabrielteste@123.com')
+        cy.get('#password').type('teste123456')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, gabrielteste (não é gabrielteste? Sair)')
+    })
+
+})
